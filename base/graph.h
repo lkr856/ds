b66b4df6132537d0ac436e61b892aa265ca3ba41
lkr856 <1044878292@qq.com>
2025-04-02 15:37:38 +0800

    add graph

--- base/graph.h
+++ base/graph.h
@@ -14,16 +14,16 @@ class Graph{
     void PFS(); //优先级搜索
     
     public:
-    void bfs( Rank ); //广度优先搜索算法
-    void dfs( Rank ); //深度优先搜索算法
-    void bcc( Rank ); //基于DFS的双连通分量分解算法
-    Stack<Tv>* tSort( Rank ); //基于DFS的拓扑排序算法
-    void prim( Rank ); //最小支撑树Prim算法
-    void dijkstra( Rank ); //最短路径Dijkstra算法
-    template <typename PU> void pfs( Rank, PU ); //优先级搜索框架
+    void bfs( int ); //广度优先搜索算法
+    void dfs( int ); //深度优先搜索算法
+    void bcc( int ); //基于DFS的双连通分量分解算法
+    Stack<Tv>* tSort( int ); //基于DFS的拓扑排序算法
+    void prim( int ); //最小支撑树Prim算法
+    void dijkstra( int ); //最短路径Dijkstra算法
+    template <typename PU> void pfs( int, PU ); //优先级搜索框架
 }
 
 template <typename PU>
-inline void Graph::pfs(Rank, PU)
+inline void Graph::pfs(int, PU)
 {
 }
